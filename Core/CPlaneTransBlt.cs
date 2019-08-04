6008de96302e367a44bc00666f47ecba4a50fb49
mifumi323 <mifumi323@gmail.com>
2019-08-04 17:00:42 +0900

    CPlaneTransBlt追加
    多分将来的に要らなくなるやつ。

---
+++ Core/CPlaneTransBlt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MifuminSoft.funyan.Core
+{
+    public class CPlaneTransBlt
+    {
+        public static Action<CDIB32, CDIB32, int, int, int> MirrorBlt1 { get; set; }
+        public static Action<CDIB32, CDIB32, int, int, int> MirrorBlt2 { get; set; }
+        public static Action<CDIB32, CDIB32, int, int, int> FlushBlt1 { get; set; }
+    }
+}
