de6874fd3166ef1fd81eda5471555f91f64a77ff
mifumi323 <mifumi323@gmail.com>
2021-10-26 22:42:54 +0900

    CApp.theAppを非null化(というより、使用前に非null初期化必須化)
    #2

--- Core/App.cs
+++ Core/App.cs
@@ -1,11 +1,15 @@
-﻿namespace MifuminSoft.funyan.Core
+﻿using System;
+
+namespace MifuminSoft.funyan.Core
 {
     public abstract class CApp
     {
-        public static CApp theApp { get; set; }
+        private static CApp? m_theApp;
+
+        public static CApp theApp { get => m_theApp ?? throw new Exception("CApp.theAppは使用前に非nullで初期化する必要があります。"); set => m_theApp = value; }
 
         public abstract CBGMBase GetBGM();
         public abstract int random(int v);
-        public abstract bool MakeFileName(out string filename,string v1,int m_RecordNumber,bool v2);
+        public abstract bool MakeFileName(out string filename, string v1, int m_RecordNumber, bool v2);
     }
 }
