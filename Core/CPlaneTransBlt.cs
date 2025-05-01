15c5225dff40474d8f242eab0b0ce8583cb5c968
mifumi323 <mifumi323@gmail.com>
2025-05-01 23:35:50 +0900

    CPlaneTransBlt.csにTODOコメントを追加
    ビルド時に代入忘れを検出できるようにするためのTODO項目を追加しました(#4)。

--- Core/CPlaneTransBlt.cs
+++ Core/CPlaneTransBlt.cs
@@ -2,6 +2,7 @@
 
 namespace MifuminSoft.funyan.Core
 {
+    // TODO: 代入忘れをビルド時に気付けるようにしたい(#4)
     public class CPlaneTransBlt
     {
         public static Action<CDIB32, CDIB32, int, int, int> MirrorBlt1 { get; set; } = (src, dst, x, y, v) => throw new NotImplementedException();
