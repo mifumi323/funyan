d41f7887180af766c848405c92aeae53ccbd7eae
mifumi323 <mifumi323@gmail.com>
2025-05-01 23:20:39 +0900

    m_pPlayerStateをnull許容型に変更
    m_pPlayerStateフィールドを`Cf3ReplayPlayerState?`に変更し、nullの可能性を考慮しました。
    Replayメソッド、OnDrawメソッド、GetMapメソッド内でnull許容演算子`!`を追加し、nullチェックを行わずにメソッドを呼び出すようにしました。

--- Core/Replay.cs
+++ Core/Replay.cs
@@ -33,7 +33,7 @@ namespace MifuminSoft.funyan.Core
             public int oldgravity;
             public int oldhyper;
         }
-        protected Cf3ReplayPlayerState m_pPlayerState;
+        protected Cf3ReplayPlayerState? m_pPlayerState;
         protected string m_FileName;
 
         // 共通
@@ -161,11 +161,11 @@ namespace MifuminSoft.funyan.Core
             if (Finished()) return;
             Cf3GameInput.ReplayInput.pressed = m_keyPressed[m_nProgress];
             Cf3GameInput.ReplayInput.pushed = m_keyPushed[m_nProgress];
-            m_pPlayerState.map!.OnMove();
-            m_pPlayerState.map!.OnPreDraw();
+            m_pPlayerState!.map!.OnMove();
+            m_pPlayerState!.map!.OnPreDraw();
             Progress();
         }
-        public void OnDraw(CDIB32 lp) { m_pPlayerState.map!.OnDraw(lp); }
-        public Cf3Map GetMap() { return m_pPlayerState.map!; }
+        public void OnDraw(CDIB32 lp) { m_pPlayerState!.map!.OnDraw(lp); }
+        public Cf3Map GetMap() { return m_pPlayerState!.map!; }
     }
 }
